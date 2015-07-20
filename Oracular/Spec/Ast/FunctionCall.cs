437d20a2fd9381c9f6cfcc9435d7a0f1f1aa29f3
Andrew Couch <andy@couchand.com>
2015-07-20 16:50:09 -0400

    fn call ast node

---
+++ Oracular/Spec/Ast/FunctionCall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Oracular.Spec.Ast
+{
+	public class FunctionCall : AstNode
+	{
+		public readonly Reference Function;
+		public readonly AstNode[] Arguments;
+
+		public FunctionCall (Reference fn, AstNode[] args)
+		{
+			this.Function = fn;
+			this.Arguments = args;
+		}
+	}
+}
+
