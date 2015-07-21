847ae59f665d24db5059bdbb7d01e5d695a8338e
Andrew Couch <andy@couchand.com>
2015-07-21 16:20:58 -0400

    initial spec config

---
+++ Oracular.Tests/SpecConfigTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+
+using Oracular;
+using Oracular.Spec.Ast;
+
+namespace Oracular.Tests
+{
+	[TestFixture]
+	public class SpecConfigTests
+	{
+		[Test]
+		public void ExpectsName ()
+		{
+			var ex = Assert.Throws<OracularException> (() => new OracularSpec (null, null, null));
+
+			Assert.That (ex.Message, Is.StringContaining ("name"));
+		}
+
+		[Test]
+		public void ExpectsTable()
+		{
+			var ex = Assert.Throws<OracularException> (() => new OracularSpec ("Foobar", null, null));
+
+			Assert.That (ex.Message, Is.StringContaining ("table"));
+		}
+
+		[Test]
+		public void ExpectsSpec()
+		{
+			var ex = Assert.Throws<OracularException> (() => new OracularSpec ("Foobar", "Baz", null));
+
+			Assert.That (ex.Message, Is.StringContaining ("spec"));
+		}
+
+		[Test]
+		[TestCase("isManager", "User", "User.Type = 'Manager'")]
+		[TestCase("isCustomer", "Account", "Account.Type = \"Customer\"")]
+		[TestCase("customerWithManagerOwner", "Account", "isCustomer(Account) AND isManager(Account.Owner)")]
+		[TestCase("managerWithCustomers", "User", "isManager(User) AND ANY(isCustomer(Account))")]
+		public void ParseSpec(string name, string table, string source)
+		{
+			var spec = new OracularSpec (name, table, source);
+
+			Assert.AreEqual (name, spec.Name);
+			Assert.AreEqual (table, spec.Table);
+			Assert.AreEqual (source, spec.Source);
+		}
+	}
+}
+
--- Oracular/OracularSpec.cs
+++ Oracular/OracularSpec.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 
+using Oracular.Spec;
+using Oracular.Spec.Ast;
+
 namespace Oracular
 {
 	public class OracularSpec
 	{
-		public OracularSpec ()
+		public readonly string Name;
+		public readonly string Table;
+		public readonly string Source;
+		public readonly AstNode Spec;
+
+		public OracularSpec (string name, string table, string spec)
 		{
+			if (name == null)
+			{
+				throw new OracularException ("spec config requires name");
+			}
+			this.Name = name;
+
+			if (table == null)
+			{
+				throw new OracularException ("spec config requires table");
+			}
+			this.Table = table;
+
+			if (spec == null)
+			{
+				throw new OracularException ("spec config requires spec expression");
+			}
+			this.Source = spec;
+
+			var parser = new Parser (new StringLexer (spec));
+			this.Spec = parser.Parse ();
 		}
 	}
 }
