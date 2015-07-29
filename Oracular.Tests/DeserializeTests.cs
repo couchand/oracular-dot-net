e22d8ad293700a132132326d41221a497eecf2cd
Andrew Couch <andy@couchand.com>
2015-07-29 12:46:02 -0400

    start deserializing

---
+++ Oracular.Tests/DeserializeTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+
+using Oracular;
+
+namespace Oracular.Tests
+{
+	[TestFixture]
+	public class DeserializeTests
+	{
+		[Test]
+		public void DeserializeEmptyConfig ()
+		{
+			var config = OracularConfig.Deserialize ("{}");
+
+			Assert.That (config.Tables, Has.Count.EqualTo (0));
+			Assert.That (config.Specs, Has.Count.EqualTo (0));
+		}
+
+		[Test]
+		public void DeserializeEmptyTables ()
+		{
+			var config = OracularConfig.Deserialize ("{\"tables\":[]}");
+
+			Assert.That (config.Tables, Has.Count.EqualTo (0));
+			Assert.That (config.Specs, Has.Count.EqualTo (0));
+		}
+
+		[Test]
+		public void DeserializeEmptySpecs ()
+		{
+			var config = OracularConfig.Deserialize ("{\"specs\":[]}");
+
+			Assert.That (config.Tables, Has.Count.EqualTo (0));
+			Assert.That (config.Specs, Has.Count.EqualTo (0));
+		}
+
+		[Test]
+		public void DeserializeSimpleTable ()
+		{
+			var config = OracularConfig.Deserialize ("{\"tables\":[{\"table\":\"Foobar\",\"fields\":[{\"name\":\"Id\"}]}]}");
+
+			Assert.That (config.Tables, Has.Count.EqualTo (1));
+			Assert.That (config.Specs, Has.Count.EqualTo (0));
+
+			var foobar = config.GetTable ("Foobar");
+
+			Assert.NotNull (foobar);
+
+			Assert.That (foobar.Fields, Has.Count.EqualTo (1));
+
+			var id = foobar.GetField ("Id");
+
+			Assert.AreEqual (FieldType.String, id.Type);
+		}
+
+		[Test]
+		public void DeserializeSimpleSpec ()
+		{
+			var config = OracularConfig.Deserialize ("{\"tables\":[{\"table\":\"Foobar\",\"fields\":[{\"name\":\"Id\"}]}],\"specs\":[{\"name\":\"hasId\",\"table\":\"Foobar\",\"spec\":\"Foobar.Id != null\"}]}");
+
+			Assert.That (config.Tables, Has.Count.EqualTo (1));
+			Assert.That (config.Specs, Has.Count.EqualTo (1));
+
+			var spec = config.GetSpec ("hasId");
+
+			Assert.AreEqual ("hasId", spec.Name);
+			Assert.AreEqual ("Foobar", spec.Table);
+			Assert.AreEqual ("Foobar.Id != null", spec.Source);
+		}
+	}
+}
+
--- Oracular/OracularConfig.cs
+++ Oracular/OracularConfig.cs
@@ -1,11 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Json;
 using System.Linq;
 
 namespace Oracular
 {
 	public class OracularConfig
 	{
+		public static OracularConfig Deserialize(string source)
+		{
+			var deserialized = JsonValue.Parse (source);
+			var rootObject = deserialized as JsonObject;
+
+			if (rootObject == null)
+			{
+				throw new OracularException ("unable to deserialize configuration");
+			}
+
+			var tables = new List<OracularTable> ();
+			var specs = new List<OracularSpec> ();
+
+			if (rootObject.ContainsKey("tables"))
+			{
+				foreach (var table in rootObject["tables"])
+				{
+					var tableObject = table as JsonObject;
+					if (tableObject == null)
+					{
+						throw new OracularException ("error deserializing table");
+					}
+
+					var fields = new List<FieldConfig> ();
+					var parents = new List<ParentConfig> ();
+
+					foreach (var field in tableObject["fields"])
+					{
+						var fieldObject = field as JsonObject;
+						if (fieldObject == null)
+						{
+							var message = String.Format ("error deserializing field on table {0}", (string)tableObject ["table"]);
+							throw new OracularException (message);
+						}
+
+						var type = FieldType.String;
+
+						if (fieldObject.ContainsKey("type"))
+						{
+							switch ((string)fieldObject["type"])
+							{
+							case "number":
+								type = FieldType.Number;
+								break;
+							case "boolean":
+								type = FieldType.Boolean;
+								break;
+							case "date":
+								type = FieldType.Date;
+								break;
+							}
+						}
+
+						fields.Add(new FieldConfig ((string)fieldObject["name"], type));
+					}
+
+					if (tableObject.ContainsKey("parents"))
+					{
+						foreach (var parent in tableObject["parents"])
+						{
+							var parentObject = parent as JsonObject;
+							if (parentObject == null)
+							{
+								var message = String.Format ("error deserializing parent on table {0}", (string)tableObject ["table"]);
+								throw new OracularException (message);
+							}
+
+							parents.Add (new ParentConfig ((string)parentObject["name"], (string)parentObject["table"], (string)parentObject["id"]));
+						}
+					}
+
+					var id = tableObject.ContainsKey ("id") ? (string)tableObject ["id"] : "Id";
+
+					tables.Add(new OracularTable ((string)tableObject["table"], id, parents, fields));
+				}
+			}
+
+			if (rootObject.ContainsKey("specs"))
+			{
+				foreach (var spec in rootObject["specs"])
+				{
+					var specObject = spec as JsonObject;
+					if (specObject == null)
+					{
+						throw new OracularException ("error deserializing spec");
+					}
+
+					specs.Add (new OracularSpec ((string)specObject["name"], (string)specObject["table"], (string)specObject["spec"]));
+				}
+			}
+
+			return new OracularConfig (tables, specs);
+		}
+
 		private Dictionary<string, OracularTable> tables;
 		private Dictionary<string, OracularSpec> specs;
 
